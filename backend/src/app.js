2202bd0b434651c3d96815e9cb704f89cf752c21
keysKuo <nkeyskuo124@gmail.com>
2024-07-17 03:16:39 +0700

    fix: docker-compose

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,6 +17,8 @@ app.use(compression());
 app.use(
 	cors({
 		origin: configs["frontendURL"],
+		methods: ['GET', 'POST', 'PUT', 'DELETE'], // Các phương thức HTTP được cho phép
+  		allowedHeaders: ['Content-Type', 'Authorization'], 
 		credentials: true,
 	})
 );
